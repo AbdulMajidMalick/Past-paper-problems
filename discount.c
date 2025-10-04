357aeafdd85c2b304edb33da5646d7371f6aeca3
AbdulMajidMalick <majidmalick2007@gmail.com>
2025-10-04 11:36:42 +0500

    Create discount.c

---
+++ discount.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    float price;
+    printf("Enter price of item");
+    scanf("%f",&price);
+    printf("\n price of item on monday:");
+    price=price*0.9;
+    printf("\nprice=%.2f",price);
+     printf("\n price of item on tuesday:");
+    price=price*0.9;
+    printf("\nprice=%.2f",price);
+     printf("\n price of item on wednesday:");
+    price=price*0.9;
+    printf("\nprice=%.2f",price);
+     printf("\n price of item on thursday:");
+    price=price*0.9;
+    printf("\nprice=%.2f",price);
+     printf("\n price of item on friday:");
+    price=price*0.9;
+    printf("\nprice=%.2f",price);
+    
+    
+    return 0;
+}
