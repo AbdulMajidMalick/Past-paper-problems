2b2f870fef46eae225e8927ab8869b6a349eebb2
AbdulMajidMalick <majidmalick2007@gmail.com>
2025-10-05 18:48:25 +0500

    Update highest_salary.c

--- highest_salary.c
+++ highest_salary.c
@@ -1,19 +1,20 @@
 #include<stdio.h>
 int main(){
     int arr[10];
-    for(int i=1;i<=10;i++){
-        printf("enter salary %d employee",i);
-        scanf("%d",arr[i]);
-        printf("salary of %d employee is",arr[i]);
+    for(int i=0;i<=9;i++){
+        printf("enter salary %d employee\n",i+1);
+        scanf("%d",&arr[i]);
+        printf("salary of number %d employee is %d\n",i+1,arr[i]);
     }
-    for(int j=1;j<=10;j++){
-        if(arr[j]>arr[j+1]){
-           int temp;
-           temp=arr[j];
-           arr[j+1]=arr[j];
-           arr[j]=temp;
+    for(int i=0;i<=9;i++){
+	
+    for(int j=1;j<=9;j++){
+        if(arr[j]>arr[0]){
+           arr[0]=arr[j];
+           arr[j]=arr[0];
         }
-        printf("highest salary so far is %d",arr[1]);
+		}
     }
+        printf("highest salary so far is %d",arr[0]);
     return 0;
 }
